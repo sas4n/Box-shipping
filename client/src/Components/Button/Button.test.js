5d0791ef1e97ab3e938c4a40ab9696fbdcb04ad2
Rahim Pormah <sas4n.porm4h@gmail.com>
2022-05-06 23:15:06 +0200

    button test added

--- client/src/Components/Button/Button.test.js
+++ client/src/Components/Button/Button.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Button from './Button'
 
 describe('Button component', () => {
+    const fakeOnChange = jest.fn()
     it('button should be rendered', () => {
         render(<Button/>)
         const button = screen.getByRole('button')
@@ -12,5 +13,12 @@ describe('Button component', () => {
         render(<Button>Save</Button>)
         const button = screen.getByRole('button')
         expect(button.textContent).toBe('Save')
+    })
+
+    it('button click event will trigger onchange function', () => {
+        render(<Button onClick = {fakeOnChange}/>)
+        const button = screen.getByRole('button')
+        fireEvent.click(button)
+        expect(fakeOnChange).toHaveBeenCalledTimes(1)
     })
 })
